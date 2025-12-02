298597d1381f28ec005c640772c8603dddc9e7c5
Phạm Tiến Phát <phamtienphat2006@gmail.com>
2025-12-02 23:44:09 +0700

    JSON

--- 5.2 JSON/index.js
+++ 5.2 JSON/index.js
@@ -19,6 +19,20 @@ app.get("/", (req, res) => {
 app.post("/recipe", (req, res) => {
   //Step 3: Write your code here to make this behave like the solution website.
   //Step 4: Add code to views/index.ejs to use the recieved recipe object.
+  switch (req.body.choice) {
+    case "chicken":
+      data = JSON.parse(recipeJSON)[0];
+      break;
+    case "beef":
+      data = JSON.parse(recipeJSON)[1];
+      break;
+    case "fish":
+      data = JSON.parse(recipeJSON)[2];
+      break;
+    default:
+      break;
+  }
+  res.redirect("/");
 });
 
 app.listen(port, () => {
